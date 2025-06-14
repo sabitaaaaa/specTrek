a0c8d5ce01d4caf6b3a49a352526c4e745c40f03
nishmi <nishmithapa01@gmail.com>
2025-06-14 11:33:42 +0545

    Add new files

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// making another route and change in the place of welcome replace it with required file name
+// Route::get('/view', function () {
+//     return view('home');
+// });
+
+
+//Short cut of putting a route
+// Route::view('/home','home');
+// Route::view("/about","about");
+
+
+// Route::get('/about', function () {
+//      return view('about');
+//  });
+// /about chai huna parcha haina url ma /about lekhda about ko page aaos bhanera 
+
+// suppose euta certain naam ko manche ko appear huna paryore data the we should:
+Route::get('/about/{name}', function ($name) {
+    echo "$name";//first way to show name
+     return view('about',["name"=>$name]);//second way to show name
+ });
+ Route::get('/weather', [WeatherController::class, 'getWeather']);
+ Route ::get('/Tours',function(){
+    return view('Tours');
+ });
